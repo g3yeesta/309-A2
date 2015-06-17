2ab3707e99c643136967a8ae7deb4ec82953d380
g3yeesta <hong.yee@mail.utoronto.ca>
2015-06-17 02:53:34 -0400

    more buttons

--- function.js
+++ function.js
@@ -1,3 +1,22 @@
+
 function play() {
-	document.getElementById("startmenu").innerHTML = "Hello World";
+	document.getElementById("startmenu").style.display = "none";
+	document.getElementById("pause").style.display = "block";
+}
+function pause(){
+	document.getElementById("resume").style.display = "block";
+	document.getElementById("pause").style.display = "none";
+	document.getElementById("restart").style.display = "block";
+}
+
+function resume(){
+	document.getElementById("pause").style.display = "block";
+	document.getElementById("resume").style.display = "none";
+	document.getElementById("restart").style.display = "none";
+}
+
+function restart(){
+	document.getElementById("startmenu").style.display = "block";
+	document.getElementById("resume").style.display = "none";
+	document.getElementById("restart").style.display = "none";
 }
