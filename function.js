aaa5afb43232a804b310b12dfea06a6d20508515
g3yeesta <hong.yee@mail.utoronto.ca>
2015-06-17 06:52:38 -0400

    info bar added

--- function.js
+++ function.js
@@ -2,29 +2,32 @@ window.onload = pageLoad;
 
 var isPlaying = false;
 
+/*
+ *Bind buttons and variables to functions on load
+ */
+ 
 function pageLoad() {
 	document.getElementById("start").onclick = play;
-	document.getElementById("pause").onclick = pause;
-	document.getElementById("resume").onclick = resume;
-	document.getElementById("restart").onclick = restart;
+	document.getElementById("pause").onclick = pause;	
+	
+	var canvas = document.getElementById('canvas');
 } 
 
-
-
 function play() {
-	document.getElementById("startmenu").style.display = "none";
-	document.getElementById("pause").style.display = "block";
+	document.getElementById("start-page").style.display = "none";
+	document.getElementById("info-bar").style.display = "table";
 	document.getElementById("game-page").style.display = "block";
 	isPlaying = true;
 }
 function pause(){
 	if (isPlaying) {
 		isPlaying = false;
-		document.getElementById("pause").innerHTML  = "resume";
+		document.getElementById("pause").innerHTML  = "►";
 	}
 	else{
 		isPlaying = true;
-		document.getElementById("pause").innerHTML  = "pause";
+		document.getElementById("pause").innerHTML  = "||";
 	}
 	
 }
+
